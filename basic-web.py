356a37bacd64434824849bde95d527ae63517ee7
meryt <meryt@meryt.com>
2014-01-15 07:22:07 -0800

    added more logging, handle post as well as get in simple web

--- basic-web.py
+++ basic-web.py
@@ -13,6 +13,8 @@ from twisted.web.resource import IResource
 from twisted.web import server, resource
 from twisted.internet import reactor
 from twisted.web.guard import HTTPAuthSessionWrapper, BasicCredentialFactory
+from twisted.python import log
+import sys
 
 class LoggingAuthSessionResource(HTTPAuthSessionWrapper):
 
@@ -24,6 +26,10 @@ class LoggingAuthSessionResource(HTTPAuthSessionWrapper):
         else:
             print colored("BASIC [%s] - %s" % (str(datetime.datetime.utcnow()), result.getErrorMessage()), "red")
         return super(LoggingAuthSessionResource, self)._loginFailed(result)
+
+    def _login(self, credentials):
+        print colored("BASIC [%s] - Attempting login for %s" % (str(datetime.datetime.utcnow()), str(credentials)), "green")
+        return super(LoggingAuthSessionResource, self)._login(credentials)
 
     def render(self, request):
         print "%s from %s for %s (user: %s pass: %s)" % (
@@ -38,6 +44,7 @@ class PublicHTMLRealm(object):
 
         if IResource in interfaces:
             return (IResource, File("/shome/jenny/web/public_html"), lambda: None)
+        print  colored("BASIC [%s] - Could not find user" % (str(datetime.datetime.utcnow())), "red")
         raise NotImplementedError()
 
 
@@ -49,5 +56,6 @@ resource = LoggingAuthSessionResource(portal, [credentialFactory])
 site = server.Site(resource)
 portNum = 7083
 print colored("Web server using Basic auth starting up on port %d " % (portNum), 'green', attrs=["bold"])
+log.startLogging(sys.stdout)
 reactor.listenTCP(portNum, site) 
 reactor.run()
--- simple-web.py
+++ simple-web.py
@@ -9,18 +9,31 @@ class Simple(resource.Resource):
     isLeaf = True
     numRequests = 0
     
-    def render_GET(self, request):
+    def logRequest(self, request):
         self.numRequests += 1
         print colored("[%s] - %s" % (str(datetime.datetime.utcnow()), str(request)), 'cyan', attrs=["bold"])
+        print "%s from %s for %s" % (
+            colored("    Request # %d" % (self.numRequests), 'red'), colored(request.getClientIP(), 'white', attrs=["bold"]), colored(request.getAllHeaders()['host'], 'yellow', attrs=["bold"]))
+        headers = request.getAllHeaders()
+        for key in headers:
+            if key == 'via' or key == 'x-forwarded-for':
+                print colored('    ' + key + ": " + headers[key], 'blue')
 
-        print "%s from %s for %s (user: %s pass: %s)" % (
-            colored("Request # %d" % (self.numRequests), 'red'), colored(request.getClientIP(), 'white', attrs=["bold"]), colored(request.getAllHeaders()['host'], 'yellow', attrs=["bold"]), request.getUser(), request.getPassword())
-
-        for key in request.getAllHeaders():
-            print(key + ": " + request.getAllHeaders()[key])
+    def render_GET(self, request):
+        self.logRequest(request)
 
         request.setHeader("content-type", "text/plain")
         return "Processed request # " + str(self.numRequests) + "\n";
+
+    def render_POST(self, request):
+        self.logRequest(request)
+
+        request.setHeader("content-type", "text/plain")
+
+        print colored('    Situation: %s' % (request.args["situation"][0]), 'green')
+
+        return 'Processed request #%d\nYou submitted: %s' % (self.numRequests, request.args["situation"][0],)
+
 
 site = server.Site(Simple())
 portNum = 7081
