3652e8f16ea0c13178be801e2c1f0956bad3d5d9
meryt <meryt@meryt.com>
2014-04-08 08:35:29 -0700

    updated log output

--- simple-web.py
+++ simple-web.py
@@ -8,16 +8,19 @@ import datetime
 class Simple(resource.Resource):
     isLeaf = True
     numRequests = 0
-    
+
     def logRequest(self, request):
         self.numRequests += 1
         print colored("[%s] - %s" % (str(datetime.datetime.utcnow()), str(request)), 'cyan', attrs=["bold"])
-        print "%s from %s for %s" % (
-            colored("    Request # %d" % (self.numRequests), 'red'), colored(request.getClientIP(), 'white', attrs=["bold"]), colored(request.getAllHeaders()['host'], 'yellow', attrs=["bold"]))
+        print "%s %s from %s for %s" % (
+            colored("    Request # %d" % (self.numRequests), 'red'),
+            colored(request.method, 'white'),
+            colored(request.getClientIP(), 'white', attrs=["bold"]), colored(request.getAllHeaders()['host'], 'yellow', attrs=["bold"]))
         headers = request.getAllHeaders()
         for key in headers:
-            if key == 'via' or key == 'x-forwarded-for':
+            if key == 'via' or key == 'x-forwarded-for' or key == 'content-length':
                 print colored('    ' + key + ": " + headers[key], 'blue')
+        print colored(request.content.read(), "yellow")
 
     def render_GET(self, request):
         self.logRequest(request)
@@ -30,14 +33,16 @@ class Simple(resource.Resource):
 
         request.setHeader("content-type", "text/plain")
 
-        print colored('    Situation: %s' % (request.args["situation"][0]), 'green')
+        #print colored('    Situation: %s' % (request.args["situation"][0]), 'green')
+        #print request.args
 
-        return 'Processed request #%d\nYou submitted: %s' % (self.numRequests, request.args["situation"][0],)
+        #return 'Processed request #%d\nYou submitted: %s' % (self.numRequests, request.args["situation"][0],)
+        return 'Processed request #%d\n'
 
 
 site = server.Site(Simple())
 portNum = 7081
-reactor.listenTCP(portNum, site) 
+reactor.listenTCP(portNum, site)
 print "Unprotected web server, Listening on port " + str(portNum) + "\n"
 reactor.run()
 
